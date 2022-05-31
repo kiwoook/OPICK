f3d7064f80e330255b8fd04291d691e9755379f9
kiwoook <tizmfns1218@naver.com>
2022-06-01 02:21:36 +0900

    1.3(ubuntu)

--- rank_crawling.py
+++ rank_crawling.py
@@ -6,15 +6,17 @@ import time
 
 from def1 import merge_list
 
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+
+
 options = webdriver.ChromeOptions()
 
 options.headless = True
 options.add_argument("--disable-gpu")
 options.add_argument("--no-sandbox")
-options.add_argument("enable-automation")
-options.add_argument("--disable-infobars")
 options.add_argument("--disable-dev-shm-usage")
 options.add_argument("lang=ko_KR")
+options.add_argument('user-agent={0}'.format(user_agent))
 
 def ranking(*args):
     img_datas = []
